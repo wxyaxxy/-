a0dbdec65b3bb5a7a340871b6b872347d5b54682
wxymac <wyatwl@163.com>
2018-11-22 23:09:41 +0800

    add one

--- 合并区间.py
+++ 合并区间.py
@@ -5,8 +5,21 @@
 # necessary).
 #
 # 您在真实的面试中是否遇到过这个题？
-# 样例
 # Insert (2, 5) into [(1,2), (5,9)], we get [(1,9)].
 #
 # Insert (3, 4) into [(1,2), (5,9)], we get [(1,2), (3,4), (5,9)].
 
+intervals = [(1, 2), (5, 9)]
+newInterval = (2, 5)
+newInterval2 = (3, 4)
+
+
+def insert(intervals, newInterval):
+    for i in intervals:
+        if newInterval[0]==i[1]:
+            i[1]=newInterval[1]
+        if newInterval[1]==i[0]:
+            i[0]=newInterval[0]
+
+
+print(insert(intervals, newInterval))
